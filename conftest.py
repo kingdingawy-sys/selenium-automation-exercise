9ac60e98e3f183c4ef62acf2268048f55584e19a
Ibrahim <kingdingawy@gmail.com>
2025-11-12 12:54:02 +0300

    Fix: Add headless Chrome options for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -2,18 +2,28 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
     # 1. Set up the Chrome driver
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # ← مهم جدًا
+    chrome_options.add_argument("--no-sandbox")  # ← مهم جدًا
+    chrome_options.add_argument("--disable-dev-shm-usage")  # ← مهم جدًا
+    chrome_options.add_argument("--disable-gpu")  # ← مهم جدًا
+    chrome_options.add_argument("--window-size=1920,1080")  # ← حجم الشاشة
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    yield driver
+    driver.quit()
+
 
     # 2. Give the driver to the test
-    yield driver
 
     # 3. After the test finishes, close the driver
-    driver.quit()
 
 
 @pytest.fixture
@@ -22,3 +32,7 @@ def setup():
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
+
+
