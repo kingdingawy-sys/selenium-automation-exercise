d54a7f31e753583c5640a915d499d93dfda257d7
Ibrahim <kingdingawy@gmail.com>
2025-11-14 09:05:03 +0300

    Verbesserungen

--- pages/ProductPage.py
+++ pages/ProductPage.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 import time
 
 class ProductPage:
@@ -9,22 +10,39 @@ class ProductPage:
         self.url = "https://automationexercise.com/products"
 
     def go_to_products_page(self):
+        """يفتح صفحة المنتجات وينتظر تحميلها بثبات"""
         self.driver.get(self.url)
-        # نتاكد إن الصفحة حملت
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, ".productinfo"))
-        )
+        try:
+            WebDriverWait(self.driver, 25).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".productinfo"))
+            )
+        except TimeoutException:
+            # نعيد المحاولة مرة واحدة في حالة بطء الموقع
+            time.sleep(5)
+            self.driver.get(self.url)
+            WebDriverWait(self.driver, 25).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".productinfo"))
+            )
 
     def click_add_to_cart(self, product_id):
-        # نستخدم XPath بناءً على DevTools
+        """يضغط زر Add to Cart باستخدام JavaScript لضمان الثبات"""
         xpath = f"//a[@data-product-id='{product_id}' and contains(@class, 'add-to-cart')]"
-        add_button = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, xpath))
-        )
-        # نشوف لو ظهرت رسالة "added to cart"
-        self.driver.execute_script("arguments[0].click();", add_button)  # نضغط بالجافا سكريبت
-        time.sleep(3)  # نخلي الرسالة تظهر
+        try:
+            add_button = WebDriverWait(self.driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, xpath))
+            )
+            self.driver.execute_script("arguments[0].click();", add_button)
+            time.sleep(3)
+        except (TimeoutException, StaleElementReferenceException):
+            # إعادة المحاولة لو العنصر لسه مش clickable
+            time.sleep(5)
+            add_button = WebDriverWait(self.driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, xpath))
+            )
+            self.driver.execute_script("arguments[0].click();", add_button)
+            time.sleep(3)
 
     def add_product_to_cart(self, product_id):
+        """عملية كاملة لإضافة المنتج"""
         self.go_to_products_page()
         self.click_add_to_cart(product_id)
